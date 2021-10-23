dc02934465bbfba628cd6755fef92498fb09a768
kuhnn <kuhnnat000@gmail.com>
2021-10-23 11:05:33 -0700

    Add IMU test script 

---
+++ TestScript_IMU/TestScript_IMU.ino
@@ -0,0 +1,16 @@
+#include "library/ICM-20608-G.h"
+
+ICM_20608_G imu(GyroRange::G_500DEGS, AccelRange::A_4_g);
+
+void setup() {
+  SPI.begin();
+  delay(10);
+  imu.init();
+}
+
+void loop() {
+  float gx, gy, gz, ax, ay, az;
+
+  imu.readGyro(gx, gy, gz);
+  imu.readAccel(ax, ay, az);
+}
